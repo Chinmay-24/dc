270df65c62acff5331be227c476429895702a4df
Chinmay-24 <naiks.chinmay@gmail.com>
2026-01-12 21:44:31 +0530

    Change signal input method and calculate redundancy

--- hoffman.m
+++ hoffman.m
@@ -1,13 +1,12 @@
 SYM=input('Enter the symbols')
 PROB=input('Enter the probabilies')
 [DICT,AVGLEN]=huffmandict(SYM,PROB)
-SIG=randsrc(1000,1,[SYM;PROB]);
+SIG=input('Enter the signal to be transmitted')
 ENCO=huffmanenco(SIG, DICT)
 DECO=huffmandeco(ENCO,DICT)
 entropy=0
 for i=1:length(PROB)
     entropy=entropy+PROB(i)*log2(1/PROB(i))
 end
-entropy
 efficiency=entropy/AVGLEN
 redundancy=1-efficiency
